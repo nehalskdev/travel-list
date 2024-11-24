95aebcb18e560ef0e68fca2a1925a3b5f6ed1b29
nehalskdev <nikesk1999@gmail.com>
2024-11-24 19:39:15 +0530

    added initial items array in app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -23,5 +28,5 @@ function PackingList() {
   return <div className="list">LIST</div>;
 }
 function Stats() {
-  return <footer>You have X items on your list</footer>;
+  return <footer className="stats">You have X items on your list</footer>;
 }
