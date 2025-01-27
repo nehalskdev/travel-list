42f21f3dbcc492a06e0e756780e7050660ce892a
nehalskdev <nikesk1999@gmail.com>
2025-01-27 14:40:26 +0530

    removed initial items array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { useState } from "react";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-// ];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
@@ -24,7 +19,8 @@ export default function App() {
     );
   }
   function handleClearListItem() {
-    setItems([]);
+    const confirmed = window.confirm("Are you sure to delete them all?");
+    if (confirmed) setItems([]);
   }
 
   return (
